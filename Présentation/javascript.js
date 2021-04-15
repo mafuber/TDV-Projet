db2d981894b26d61033d2a49e1cf15ffa9e2d5c4
ruffii <jessica.ruffiner@unifr.ch>
2021-04-15 16:31:09 +0200

    Hamburger

---
+++ Présentation/javascript.js
@@ -0,0 +1,22 @@
+/*// Select all the elements that use a specified distinguisher.
+const menuTriggers = document.querySelectorAll("[data-menu-toggle]");
+// For each element, add a listener for the "click" event.
+Array.prototype.forEach.apply(menuTriggers, [
+  function(trigger) {
+    trigger.addEventListener("click", function(e) {
+      e.preventDefault(); // prevent default link behaviour
+      // Toggle the sidebar when a click is detected.
+      toggleSidebar(); // this function will be defined in a later step
+    });
+  }
+]);*/
+
+const menuIcon = document.querySelector("hamburger-menu");
+const navbar = document.querySelector("navbar");
+
+menuIcon.addEventListener("click", () => {
+  navbar.classList.toggle("change");
+});
+
+
+    
