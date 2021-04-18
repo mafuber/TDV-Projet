67a935bd61bf22b356151c78cf0f97803af3dbe2
ruffii <jessica.ruffiner@unifr.ch>
2021-04-18 18:57:59 +0200

    a

--- Présentation/javascript.js
+++ Présentation/javascript.js
@@ -1,39 +1,17 @@
-/*// Select all the elements that use a specified distinguisher.
-const menuTriggers = document.querySelectorAll("[data-menu-toggle]");
-// For each element, add a listener for the "click" event.
-Array.prototype.forEach.apply(menuTriggers, [
-  function(trigger) {
-    trigger.addEventListener("click", function(e) {
-      e.preventDefault(); // prevent default link behaviour
-      // Toggle the sidebar when a click is detected.
-      toggleSidebar(); // this function will be defined in a later step
-    });
-  }
-]);*/
-
+var counter;
+counter = 0
 
 function out(a){
   console.log(a.classList, "lkjsfdgäl")
   a.classList.toggle("change"); /*Get the current element (this) and toggle the class between the "current" class und change (ajoute le class change apres lautre class)*/
- /*next = document.getElementsByClassName("button_next")
-  next.style.right="200px"; */
-  document.getElementById("button_next").style.right= "200px"
+
+if ((counter % 2) == 0) {
+document.getElementById("button_next").style.right= "200px";}
+else{
+document.getElementById("button_next").style.right ="0px";
 
 }
-
-/*
-const menuIcon = document.querySelector(".hamburger-menu");
-const navbar = document.querySelector(".navbar");
-
-menuIcon.addEventListener("click", () => {
-  navbar.classList.toggle("change");
-  console.log("lkdsjf")
-})
-
-/*
-menuIcon.addEventListener("click", () => {
-  navbar.classList.toggle("change");
-});
-*/
+counter += 1
+}
 
     
