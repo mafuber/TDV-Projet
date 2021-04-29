e22021cc995389ea3f4321d023bbc6a7a019a00c
ruffii <jessica.ruffiner@unifr.ch>
2021-04-29 21:03:56 +0200

    Kontakt

--- Présentation/javascript.js
+++ Présentation/javascript.js
@@ -2,7 +2,7 @@ var counter;
 counter = 0
 
 function out(a){
-  console.log(a.classList, "lkjsfdgäl")
+
   a.classList.toggle("change"); /*Get the current element (this) and toggle the class between the "current" class und change (ajoute le class change apres lautre class)*/
 
 if ((counter % 2) == 0) {
