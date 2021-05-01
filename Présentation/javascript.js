324f8f480f1d59e2ee49b57e872766443a64b7dc
ivan.garciainguil@unifr.ch <ivan.garciainguil@unifr.ch>
2021-05-01 16:43:35 +0200

    Ivan corrections

--- Présentation/javascript.js
+++ Présentation/javascript.js
@@ -14,13 +14,14 @@ function out(a) {
     counter += 1
 }
 
-var width = 500;
-var height = 500;
+var width = 300;
+var height = 300;
 
-var text2 = d3.json("../../Data_vis/mydata.json", function(data) {
-    var canvas = d3.select("body").append("svg").attr("width", width).attr("height", height);
-    var imag = d3.select("svg").append("image").attr("xlink:href", "../Pictures/Merkur.jpg").attr("width", width).attr("height", 200);
+var info = d3.json("../../Data_vis/mydata.json", function(data) {
+    var canvas = d3.select(".centered_container").append("svg").attr("width", width).attr("height", height);
+    var imag = canvas.append("image").attr("xlink:href", "../Pictures/Merkur.jpg").attr("width", width).attr("height", height);
 
+    //var text =
     canvas.on("mouseover", function(d) {
             canvas.selectAll("text").remove()
             canvas.selectAll("image").remove()
@@ -28,29 +29,29 @@ var text2 = d3.json("../../Data_vis/mydata.json", function(data) {
                 .data(data.Mercury)
                 .enter()
                 .append("text")
-                .attr("y", function(d, i) { return i * 50 + 100; })
-                .attr("fill", "white")
+                .attr("y", function(d, i) { return i * 50 + 20; })
+                .attr("fill", "black")
                 .text(function(d) {
-                    return "Masse : " + d.mass + "/n" +
-                        "</n>" + " Diamètre : " + d.diameter + "/n" + "</br>" +
-                        "<br>" + " autres dimensions " + "</br>";
+                    return "Juste pour voir si ça marche " + d.mass;
+                    // " &#xA;" +
+                    //   " Diamètre : " + d.diameter + "<br/>" +
+                    // " autres dimensions " + "<br/>";
                 })
             d3.select(this)
                 .style("cursor", "pointer")
                 .transition()
                 .duration(200)
                 .style("opacity", 1);
-
-
         })
         .on("mouseout", function(d) {
             canvas.selectAll("text").remove()
             canvas.selectAll("image").remove()
+
             d3.select(this)
                 .append("image")
                 .attr("xlink:href", "../Pictures/Merkur.jpg")
                 .attr("width", width)
-                .attr("height", 200)
+                .attr("height", height)
                 .style("cursor", "")
                 .transition()
                 .duration(200)
