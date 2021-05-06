4a15594c02fc8f4664816ba3c48559939474d56f
ruffii <jessica.ruffiner@unifr.ch>
2021-05-06 14:17:58 +0200

    Donut

--- Présentation/Planets/Graf_Planets.js
+++ Présentation/Planets/Graf_Planets.js
@@ -1,157 +1,79 @@
 
-    var salesData;
-    var truncLengh = 30;
 
-    function ready () {
-        Plot();
-    }
+// set the dimensions and margins of the graph
+var width = 450
+var height = 450
+var margin = 40
 
-    function Plot() {
-        d3.json("mydata.json", function(data){console.log(data)})
-        //then(function (data){
-       
-      //  })
-        TransformChartData(chartData, chartOptions);
-        BuildPie("chart", chartData, chartOptions);
-    }
+// The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+var radius = Math.min(width, height) / 2 - margin
 
-    function BuildPie(id, chartData, options) {
-        var xVarName;
-        var divisionRatio = 2.5;
-        var legendoffset = 0;
+// append the svg object to the div called 'my_dataviz'
+var svg = d3.select("#my_dataviz")
+  .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-        chart = d3.select("#" + id + " .innerCont");
+// Create dummy data
 
-        var yVarName = options[0].yaxis;
-        width = $(chart[0]).outerWidth(),
-        height = $(chart[0]).outerHeight(),
-        radius = Math.min(width, height) / divisionRatio;
+//var data1 = {a: 9, b: 20, c:30, d:8, e:12}
+//var data2 = ["carbonDioxide","nitrogen", "oxgygen","argon","methane","sodium","hydrogen","helium","other"]
 
-        xVarName = options[0].xaxis;
+d3.json("data.json", function(data){
+    var planet = document.getElementsByTagName("H1")[1].getAttribute("id")  //der aktuelle Planet
+    var Werte = data[planet][0]
+    //var Komponenten = data[planet][0].carbonDioxide
+    //for (element in data2){
+        //var new1 = eval(data2[element])
+      //  console.log(Werte.carbonDioxide)
+
+       // console.log(Werte.data2[element])
+    //}
+    //var Datas = [Werte.carbonDioxide, Werte.nitrogen, Werte.argon, Werte.methane, Werte.sodium, Werte.hydrogen, Werte.helium, Werte.other]
+
+    var Datas = {"CarbonDioxid": Werte.carbonDioxide, "nitrogen": Werte.nitrogen, "oxygen": Werte.argon, "methan": Werte.methane, "sodium" : Werte.sodium, "hydrogen" : Werte.hydrogen, "helium" : Werte.helium, "other": Werte.other}
+
+  
+
+// set the color scale
+var color = d3.scaleOrdinal()
+  .domain([0, d3.max(Object.values(Datas))])
+  .range(["#ba0298", "#a802ba", "#8902ba", "#7c02ba", "#6a02ba", "#5902ba", "#4102ba", "#2c02ba"])
+
+// Compute the position of each group on the pie:
 
 
+var pie = d3.pie()
+  .value(function(d){ 
+      return d.value;})
+      //console.log(Datas[1]); return Datas[1]})
+      
+    /*function(){
+      for (i = 0; i < Datas.length; i++ ){
+          counter += 1
+          console.log(Datas[i], i, counter)}
+      return Datas[i]
         
-        var rcolor = d3.scale.ordinal().range(runningColors);
+  //  return d.value; 
+  })
+  */
+var data_ready = pie(d3.entries(Datas))
 
-        arc = d3.svg.arc()
-                .outerRadius(radius)
-                .innerRadius(radius - 200);
+// Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+svg
+  .selectAll("div")
+  .data(data_ready)
+  .enter()
+  .append('path')
+  .attr('d', d3.arc()
+    .innerRadius(width / 5)         // This is the size of the donut hole
+    .outerRadius(radius))
+  .attr("fill", function(d){ console.log("jdkjflkds"); return (color(d.data.key)) })
+  .attr("stroke", "black")
+  .style("stroke-width", "2px")
+  .style("opacity", 0.7)
 
-        chart = chart
-                .append("svg")  //append svg element inside #chart
-                .attr("width", width)    //set width
-                .attr("height", height)  //set height
-                .append("g")
-                .attr("transform", "translate(" + (width / divisionRatio) + "," + ((height / divisionRatio) + 30) + ")");
-
-        var pie = d3.layout.pie()
-                    .sort(null)
-                    .value(function (d) {
-                        return d.Total;
-                    });
-
-        var g = chart.selectAll(".arc")
-                    .data(pie(runningData))
-                    .enter().append("g")
-                    .attr("class", "arc");
-
-        var count = 0;
-
-        var path = g.append("path")
-                    .attr("d", arc)
-                    .attr("id", function (d) { return "arc-" + (count++); })
-                    .style("opacity", function (d) {
-                        return d.data["op"];
-                    });
-
-        path.append("svg:title")
-        .text(function (d) {
-            return d.data["title"] + " (" + d.data[yVarName] + ")";
-        });
-
-        path.style("fill", function (d) {
-            return rcolor(d.data[xVarName]);
-        })
-
-        g.append("text")
-         .attr("transform", function (d) { return "translate(" + arc.centroid(d) + ")"; })
-         .attr("dy", ".35em")
-         .style("text-anchor", "middle")
-         .style("opacity", 1)
-         .text(function (d) {
-             return d.data[yVarName];
-         });
-
-
-        count = 0;
-        var legend = chart.selectAll(".legend")
-            .data(runningData).enter()
-            .append("g").attr("class", "legend")
-            .attr("legend-id", function (d) {
-                return count++;
-            })
-            .attr("transform", function (d, i) {
-                return "translate(15," + (parseInt("-" + (runningData.length * 10)) + i * 28 + legendoffset) + ")";
-            })
-            .style("cursor", "pointer")
-
-        var leg = legend.append("rect");
-
-        leg.attr("x", width / 2)
-            .attr("width", 18).attr("height", 18)
-            .style("fill", function (d) {
-                return rcolor(d[yVarName]);
-            })
-        legend.append("text").attr("x", (width / 2) - 5)
-            .attr("y", 9).attr("dy", ".35em")
-            .style("text-anchor", "end").text(function (d) {
-                return d.caption;
-            });
-
-        leg.append("svg:title")
-        .text(function (d) {
-            return d["title"] + " (" + d[yVarName] + ")";
-        });
-
-    }
-
-    function TransformChartData(chartData, opts) {
-        var result = [];
-        var resultColors = [];
-        var counter = 0;
-        var hasMatch;
-        var xVarName;
-        var yVarName = opts[0].yaxis;
-
-        xVarName = opts[0].xaxis;
-
-        for (var i in chartData) {
-            hasMatch = false;
-            for (var index = 0; index < result.length; ++index) {
-                var data = result[index];
-
-                if (data[xVarName] == chartData[i][xVarName]) {
-                    result[index][yVarName] = result[index][yVarName] + chartData[i][yVarName];
-                    hasMatch = true;
-                    break;
-                }
-            }
-            if (hasMatch == false) {
-                ditem = {};
-                ditem[xVarName] = chartData[i][xVarName];
-                ditem[yVarName] = chartData[i][yVarName];
-                ditem["caption"] = opts[0].captions != undefined ? opts[0].captions[0][chartData[i][xVarName]] : "";
-                ditem["title"] = opts[0].captions != undefined ? opts[0].captions[0][chartData[i][xVarName]] : "";
-                result.push(ditem);
-
-                resultColors[counter] = opts[0].color != undefined ? opts[0].color[0][chartData[i][xVarName]] : "";
-
-                counter += 1;
-            }
-        }
-
-        runningData = result;
-        runningColors = resultColors;
-        return;
-    }
-
+});
+    
