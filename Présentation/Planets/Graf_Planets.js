a5d33291fa4c2c35d20d71b77cce11c162b5f54c
ruffii <jessica.ruffiner@unifr.ch>
2021-05-11 11:49:45 +0200

    tooltips

--- Présentation/Planets/Graf_Planets.js
+++ Présentation/Planets/Graf_Planets.js
@@ -1,11 +1,17 @@
 //nur anzeigen falls d kondition
 //tooltips
-//qqun a change les trucs
 
-// set the dimensions and margins of the graph
-var width = 500
-var height = 450
-var margin = 40
+
+
+const width_document  = window.innerWidth;
+const height_document = window.innerHeight;
+
+
+var width = width_document/2
+
+
+10 < width
+var margin = 20
 
 // tooltip
 /*var tooli = d3.select("body").append("div")
@@ -14,15 +20,15 @@ var margin = 40
     ;*/
 
 // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
-var radius = Math.min(width, height) / 2 - margin
+var radius = width / 2 - margin
 
 // append the svg object to the div called 'my_dataviz'
 var svg = d3.select("#my_dataviz")
   .append("svg")
     .attr("width", width)
-    .attr("height", height)
+    .attr("height", width)
   .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+    .attr("transform", "translate(" + width / 2 + "," + width / 2 + ")");
 
 // Create dummy data
 
@@ -42,7 +48,7 @@ d3.json("data.json", function(data){
     //var Datas = [Werte.carbonDioxide, Werte.nitrogen, Werte.argon, Werte.methane, Werte.sodium, Werte.hydrogen, Werte.helium, Werte.other]
 
     var Datas = {"CarbonDioxid": Werte.carbonDioxide, "nitrogen": Werte.nitrogen, "oxygen": Werte.argon, "methan": Werte.methane, "sodium" : Werte.sodium, "hydrogen" : Werte.hydrogen, "helium" : Werte.helium, "other": Werte.other}
-
+  
 
 // set the color scale
 var color = d3.scaleOrdinal()
@@ -84,12 +90,36 @@ svg
   .attr("stroke", "black")
   .style("stroke-width", "2px")
   .style("opacity", 0.7)
+  
   .on("mouseover", function(d) {
     d3.select(this)   // DAS AKTUELLE ELEMENT AUSWàHLEN  oder “click”
 .attr("fill", "orange");
-d3.select(this).append("text")
+ // makes the tooltip appear on mouseover
+    console.log("Happy Birthday")
+ d3.select(this)
+   .style("stroke", "white")
+   .style("stroke-width", "3px")
+   d3.select("body")
+   .append("div")
+   .attr("class", "tooltip")
+   .style("opacity", 0)
+   .style("position", "absolute").transition()
+   .duration(200)
+   .style("opacity", .9);
+   d3.select("body")
+   .append("div")
+   .attr("class", "tooltip")
+   .style("opacity", 1)
+   .style("position", "absolute")
+   .style("fill", "black")
+   .style("background-color", "black")
+   .html( d.data.key.charAt(0).toUpperCase()+d.data.key.slice(1) +":" + d.value * 100 +"%")
+   .style("left", (d3.event.pageX) + "px")
+   .style("top", (d3.event.pageY - 28) + "px")
+   .style("padding", "10px");})
+/*.append("text")
        .attr("id", "tooltip")
-        .attr("x",  String(event.pageX)) //Get the x values of the mouse
+        .attr("x",  String(event.pageX) /2) //Get the x values of the mouse
        .attr("y", String(event.pageY))
        .attr("height", "90px")
        .attr("width", "20px")
@@ -100,11 +130,34 @@ d3.select(this).append("text")
        
        .text("kdjfl")
        //.style("fill", "white");
-       ;
+       ;*/
 
-
-   
-    /*//Create the tooltip label
+       
+/*
+        //Create the tooltip label
+        svg.append("div")
+           .attr("id", "tooltip")
+           .attr("x", event.pageX )
+           .attr("y", event.pageY)
+           .attr("text-anchor", "middle")
+           .attr("font-family", "sans-serif")
+           .attr("font-size", "11px")
+           .attr("font-weight", "red")
+           .attr("fill", "black")
+           .text("saöldkfaödgfäldskjgäaskdlflsadfjälkdsajfsadkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk");
+*/
+       /*
+       .on("mouseout", function() {
+       
+        //Remove the tooltip
+        d3.select("#tooltip").remove();
+        
+       })
+       .on("click", function() {
+           sortBars();
+       });
+   /*
+    //Create the tooltip label
     d3.select(this)
         .append("text") 
        .attr("id", "tooltip")
@@ -119,9 +172,23 @@ d3.select(this).append("text")
        .attr("fill", "black")
        .attr("background-color", "red")
        .text(343295);
-       console.log(d.value, d.key, event.pageX);
-*/
-})
+       console.log(d.value, d.key, event.pageX);*/
+
+       .on("mouseout", function (d) {
+         console.log("ldskjf")
+
+        d3.select(this)
+        .attr("fill", function(d){return (color(d.data.key))})
+        .style("stroke-width", "2px")
+        .style("stroke", "black")
+        //makes the tooltip disappear on mouseout
+  
+        d3.selectAll(".tooltip")
+        .style("opacity", 0)
+  
+      })
+
+      /*
 .on("mouseout", function() {
 
     //Remove the tooltip
@@ -129,7 +196,7 @@ d3.select(this).append("text")
     d3.select(this).attr("fill", function(d){return (color(d.data.key)) ;}) 
     
 })
-
+*/
   
   
 // The arc generator
@@ -160,7 +227,7 @@ svg
 .enter()
 .append("polyline") //function(d){ d < 0.2? a = "": a = "polyline"; return a})
   .attr("stroke", "black")
-  .attr("opacity", function(d){d.value > 0.1? a = "200%":  a = "0%"; 
+  .attr("opacity", function(d){d.value > 0.016? a = "200%":  a = "0%"; 
   return a
   /*function (d){    
       if (d.value > 0.01){
@@ -180,9 +247,10 @@ svg
     var posB = outerArc.centroid(d) // line break: we use the other arc generator that has been built only for that
     var posC = outerArc.centroid(d); // Label position = almost the same as posB
     var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2 // we need the angle to see if the X position will be at the extreme right or extreme left
-    posC[0] = radius * 0.95 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
+    posC[0] = radius * 0.8 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
     return [posA, posB, posC]
   })
+
 
 
 // Add the keys(label)
@@ -196,13 +264,50 @@ svg
   .attr('transform', function(d) {
       var pos = outerArc.centroid(d);
       var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
-      pos[0] = radius * 0.99 * (midangle < Math.PI ? 1 : -1);
+      pos[0] = radius * 0.8 * (midangle < Math.PI ? 1 : -1);
       return 'translate(' + pos + ')';
   })
-  .attr("opacity", function(d){d.value > 0.1? a = "200%":  a = "0%"; 
+  .attr("opacity", function(d){d.value > 0.016? a = "200%":  a = "0%"; 
   return a})
   .style('text-anchor', function(d) {
       var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
       return (midangle < Math.PI ? 'start' : 'end')
   })
+  
+  .style("font-size", "15px")
 })
+
+  // Creation of the SVG
+ /* d3.select("svg")
+    .selectAll(".p")
+    .data(planets)
+    .enter()
+    .append("circle")
+    .attr("class", "p")
+    .attr("cx", function (d) { return parseInt(d.cx) + '%' })
+    .attr("r", function (d) { return parseInt(d.r) })
+    .attr("fill", function (d) { return "url(#" + d.name.toLowerCase() + ")" })
+    // .attr("transform", function (d) { if (isNaN(d.x)) { return ""; } else { return "translate(" + posScale(parseFloat(d.x)) + "," + posScale(parseFloat(d.y)) + ")" } })
+    .on("mouseover", function (d) {
+      // makes the tooltip appear on mouseover
+      console.log("mouseover")
+      d3.select(this)
+        .style("stroke", "white")
+        .style("stroke-width", 1)
+      div.transition()
+        .duration(200)
+        .style("opacity", .9);
+      div.html("Nom: " )
+        .style("left", (d3.event.pageX) + "px")
+        .style("top", (d3.event.pageY - 28) + "px");
+
+    })
+    .on("mouseout", function (d) {
+      d3.select(this)
+        .style("stroke-width", 0)
+      //makes the tooltip disappear on mouseout
+
+      div.transition()
+        .duration(200)
+        .style("opacity", 0);
+    })*/
