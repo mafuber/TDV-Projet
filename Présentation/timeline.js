8337374ff3e0456823f2a4f4905fc3a5f88cc19e
kira-lu <82097047+kira-lu@users.noreply.github.com>
2021-05-13 14:57:45 +0200

    links

--- Présentation/timeline.js
+++ Présentation/timeline.js
@@ -60,8 +60,6 @@ d3.json('../DATA/json/planets.json', function (error, planets) {
       d3.selectAll(".p").attr("transform", function (d) { if (isNaN(d.x)) { return ""; } else { return "translate(" + posScale(parseFloat(d.x)) + "," + posScale(parseFloat(d.y)) + ")" } });
 
     }
-
-
   }
   // Creation of the SVG
   d3.select("svg")
@@ -73,6 +71,14 @@ d3.json('../DATA/json/planets.json', function (error, planets) {
     .attr("cx", function (d) { return parseInt(d.cx) + '%' })
     .attr("r", function (d) { return parseInt(d.r) })
     .attr("fill", function (d) { return "url(#" + d.name.toLowerCase() + ")" })
+    .attr("xlink:href", function(d){if(d.name == "Mercury"){return "1_Mercure.html"}})
+    .attr("xlink:href", function(d){if(d.name == "Venus"){return "2_Venus.html"}})
+    .attr("xlink:href", function(d){if(d.name == "Earth"){return "3_Terre.html"}})
+    .attr("xlink:href", function(d){if(d.name == "Mars"){return "4_Mars.html"}})
+    .attr("xlink:href", function(d){if(d.name == "Jupiter"){return "5_Jupiter.html"}})
+    .attr("xlink:href", function(d){if(d.name == "Saturn"){return "6_Saturne.html"}})
+    .attr("xlink:href", function(d){if(d.name == "Uranus"){return "7_Uranus.html"}})
+    .attr("xlink:href", function(d){if(d.name == "Neptune"){return "8_Neptune.html"}})
     // .attr("transform", function (d) { if (isNaN(d.x)) { return ""; } else { return "translate(" + posScale(parseFloat(d.x)) + "," + posScale(parseFloat(d.y)) + ")" } })
     .on("mouseover", function (d) {
       // makes the tooltip appear on mouseover
