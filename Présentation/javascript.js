da7df9653bdc3dac540070106380c3288f5b39c8
ivan.garciainguil@unifr.ch <ivan.garciainguil@unifr.ch>
2021-05-14 19:04:27 +0200

    a voir avec les autres si ca joue

--- Présentation/javascript.js
+++ Présentation/javascript.js
@@ -15,11 +15,11 @@ function out(a) {
     }
     counter += 1
 }
-// demander à Jessica s'il faut effacer la fonction d'avant ou pas?
 
-var canvas = d3.select(".Mercure").append("svg").attr("viewBox", "0 0 700 350").attr("width", "50%").attr("height", "50%");;
+var canvas = d3.select(".Mercure").append("svg").attr("viewBox", "0 0 700 350").attr("width", "100%").attr("height", "100%");
 
 function planeteswap(planeteName) {
+    canvas.select("image").remove();
     var info = d3.json("../../Data_vis/ivandata.json", function(data) {
 
 
@@ -27,39 +27,45 @@ function planeteswap(planeteName) {
 
         canvas.on("mouseover", function(d) {
                 canvas.selectAll("text").remove()
-                canvas.selectAll("image").attr("transform", "translate(-200,120)rotate(-90)");
+                canvas.selectAll("image").transition().duration(2000).attr("transform", "translate(175,0)scale(0.5,0.5)");
                 var texto = canvas.selectAll("text")
                     .data(data[planeteName])
                     .enter()
 
                 //on n'arrivait pas à effectuer une seule formule avec l'assistant
                 texto.append("text")
-                    .attr("dy", "1em")
+                    .attr("dy", "13em")
+                    .attr("x", "" + width / 4 + "")
                     .text(function(d) {
                         return "Masse de la planète : " + d.mass + " x 10^(24) kg";
                     })
                 texto.append("text")
-                    .attr("dy", "2em")
+                    .attr("dy", "14em")
+                    .attr("x", "" + width / 4 + "")
                     .text(function(d) {
                         return "Diamètre de la planète : " + d.diameter + "km"
                     })
                 texto.append("text")
-                    .attr("dy", "3em")
+                    .attr("dy", "15em")
+                    .attr("x", "" + width / 4 + "")
                     .text(function(d) {
                         return "Densité de la planète : " + d.density + " g/dm^3"
                     })
                 texto.append("text")
-                    .attr("dy", "4em")
+                    .attr("dy", "16em")
+                    .attr("x", "" + width / 4 + "")
                     .text(function(d) {
                         return "Mesure de la gravité : " + d.gravity + " m/s^2"
                     })
                 texto.append("text")
-                    .attr("dy", "5em")
+                    .attr("dy", "17em")
+                    .attr("x", "" + width / 4 + "")
                     .text(function(d) {
                         return "Température moyenne : " + d.meanTemperature + " °C"
                     })
                 texto.append("text")
-                    .attr("dy", "6em")
+                    .attr("dy", "18em")
+                    .attr("x", "" + width / 4 + "")
                     .text(function(d) {
                         return "Longeur d'une journée : " + d.lengthOfDay + " h"
                     })
@@ -78,7 +84,9 @@ function planeteswap(planeteName) {
                     .attr("xlink:href", "../Pictures/" + planeteName + ".jpg")
                     .attr("width", "100%")
                     .attr("height", "100%")
-                    .style("cursor", "")
+
+
+                .style("cursor", "")
                     .transition()
                     .duration(200)
                     .style("opacity", 1);
@@ -86,3 +94,4 @@ function planeteswap(planeteName) {
 
     });
 }
+planeteswap("Mercure");
