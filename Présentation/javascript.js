c716ec9f6095d665a5a36aa5008f86fbb5544b79
ivan.garciainguil@unifr.ch <ivan.garciainguil@unifr.ch>
2021-05-15 15:12:16 +0200

    Ivan

--- Présentation/javascript.js
+++ Présentation/javascript.js
@@ -21,12 +21,10 @@ var canvas = d3.select(".Mercure").append("svg").attr("viewBox", "0 0 700 350").
 function planeteswap(planeteName) {
     canvas.select("image").remove();
     var info = d3.json("../../Data_vis/ivandata.json", function(data) {
-
-
         canvas.append("image").attr("xlink:href", "../Pictures/" + planeteName + ".jpg").attr("width", "100%").attr("height", "100%");
 
         canvas.on("mouseover", function(d) {
-                canvas.selectAll("text").remove()
+                canvas.selectAll("text").remove();
                 canvas.selectAll("image").transition().duration(2000).attr("transform", "translate(175,0)scale(0.5,0.5)");
                 var texto = canvas.selectAll("text")
                     .data(data[planeteName])
@@ -34,61 +32,69 @@ function planeteswap(planeteName) {
 
                 //on n'arrivait pas à effectuer une seule formule avec l'assistant
                 texto.append("text")
-                    .attr("dy", "13em")
-                    .attr("x", "" + width / 4 + "")
+                    .attr("dy", "9em")
+                    .attr("dx", "50")
                     .text(function(d) {
-                        return "Masse de la planète : " + d.mass + " x 10^(24) kg";
+                        return "En résumé :";
                     })
                 texto.append("text")
-                    .attr("dy", "14em")
+                    .attr("dy", "9em")
+                    .attr("x", "" + width / 4 + "")
+                    .text(function(d) {
+                        return "Masse de la planète : " + d.mass + " x 10^(24) kg" +
+                            "</br>" + "a";
+                    })
+
+                texto.append("text")
+                    .attr("dy", "10em")
                     .attr("x", "" + width / 4 + "")
                     .text(function(d) {
                         return "Diamètre de la planète : " + d.diameter + "km"
                     })
                 texto.append("text")
-                    .attr("dy", "15em")
+                    .attr("dy", "11em")
                     .attr("x", "" + width / 4 + "")
                     .text(function(d) {
                         return "Densité de la planète : " + d.density + " g/dm^3"
                     })
                 texto.append("text")
-                    .attr("dy", "16em")
+                    .attr("dy", "12em")
                     .attr("x", "" + width / 4 + "")
                     .text(function(d) {
                         return "Mesure de la gravité : " + d.gravity + " m/s^2"
                     })
                 texto.append("text")
-                    .attr("dy", "17em")
+                    .attr("dy", "13em")
                     .attr("x", "" + width / 4 + "")
                     .text(function(d) {
                         return "Température moyenne : " + d.meanTemperature + " °C"
                     })
                 texto.append("text")
-                    .attr("dy", "18em")
+                    .attr("dy", "14em")
                     .attr("x", "" + width / 4 + "")
                     .text(function(d) {
                         return "Longeur d'une journée : " + d.lengthOfDay + " h"
                     })
+
                 d3.select(this)
                     .style("cursor", "pointer")
-                    .transition()
-                    .duration(200)
+                    //.transition()
+                    //.duration(2000)
                     .style("opacity", 1);
             })
             .on("mouseout", function(d) {
-                canvas.selectAll("text").remove()
-                canvas.selectAll("image").remove()
+
+                canvas.selectAll("text").remove();
+                canvas.selectAll("image").remove();
 
                 d3.select(this)
                     .append("image")
                     .attr("xlink:href", "../Pictures/" + planeteName + ".jpg")
                     .attr("width", "100%")
                     .attr("height", "100%")
-
-
-                .style("cursor", "")
+                    .style("cursor", "")
                     .transition()
-                    .duration(200)
+                    .duration(2000)
                     .style("opacity", 1);
             })
 
