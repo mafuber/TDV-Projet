3da19674de7f0ab4e31c0cd103ec1530bb5036f0
ivan.garciainguil@unifr.ch <ivan.garciainguil@unifr.ch>
2021-05-19 16:03:42 +0200

    ivan changements

--- Présentation/javascript.js
+++ Présentation/javascript.js
@@ -16,11 +16,6 @@ function out(a) {
     counter += 1
 }
 
-//var canvas = d3.select(".Mercure").append("svg").attr("viewBox", "0 0 700 400").attr("width", "100%").attr("height", "100%");
-
-
-
-
 var info = d3.json("../../Data_vis/ivandata.json", function(data) {
     var planete = document.getElementsByTagName("H1")[1].getAttribute("id");
 
@@ -35,87 +30,113 @@ var info = d3.json("../../Data_vis/ivandata.json", function(data) {
                 .data(data[planete])
                 .enter()
 
-            //on n'arrivait pas à effectuer une seule formule avec l'assistant
+            //on n'arrivait pas à effectuer une seule formule avec l'assistant : code fonctionne pas pour une formule.
 
             texto.append("text")
                 .attr("dy", "10em")
                 .attr("dx", "50")
-                .text(function(d) {
-                    return "En résumé :";
-                })
+                .attr("font-size", "21px")
+                .text("En résumé :")
             texto.append("text")
                 .attr("dy", "12em")
                 .attr("dx", "50")
+                .attr("font-size", "21px")
                 .text(function(d) {
                     return "Masse de la planète : " + d.mass + " x 10^(23) kg";
                 })
-
             texto.append("text")
                 .attr("dy", "13em")
                 .attr("dx", "50")
+                .attr("font-size", "21px")
                 .text(function(d) {
                     return "Diamètre de la planète : " + d.diameter + " km"
                 })
             texto.append("text")
                 .attr("dy", "14em")
                 .attr("dx", "50")
+                .attr("font-size", "21px")
                 .text(function(d) {
                     return "Densité de la planète : " + d.density + " g/dm^3"
                 })
             texto.append("text")
                 .attr("dy", "15em")
                 .attr("dx", "50")
+                .attr("font-size", "21px")
                 .text(function(d) {
                     return "Mesure de la gravité : " + d.gravity + " m/s^2"
                 })
             texto.append("text")
                 .attr("dy", "16em")
+                .attr("dx", "50")
+                .attr("font-size", "21px")
+                .text(function(d) {
+                    return "Vitesse de libération : " + d.escapeVelocity + " km/s^2"
+                })
+
+            texto.append("text")
+                .attr("dy", "17em")
                 .attr("x", "50")
+                .attr("font-size", "21px")
                 .text(function(d) {
                     return "Période de rotation : " + d.rotationPeriod + " jours"
                 })
             texto.append("text")
-                .attr("dy", "17em")
+                .attr("dy", "18em")
                 .attr("dx", "50")
-                .text(function(d) {
-                    return "Température moyenne : " + d.meanTemperature + " °C"
-                })
-            texto.append("text")
-                .attr("dy", "12em")
-                .attr("x", "" + width / 2 + "")
+                .attr("font-size", "21px")
                 .text(function(d) {
                     return "Longeur d'une journée : " + d.lengthOfDay + " h"
                 })
             texto.append("text")
-                .attr("dy", "13em")
+                .attr("dy", "12em")
                 .attr("x", "" + width / 2 + "")
+                .attr("font-size", "21px")
                 .text(function(d) {
                     return "Distance au soleil : " + d.distanceFromSun + " km"
                 })
             texto.append("text")
-                .attr("dy", "14em")
+                .attr("dy", "13em")
                 .attr("x", "" + width / 2 + "")
-                .text(function(d) {
-                    return "Nombre de lunes : " + d.numberOfMoons
-                })
-            texto.append("text")
-                .attr("dy", "15em")
-                .attr("x", "" + width / 2 + "")
+                .attr("font-size", "21px")
                 .text(function(d) {
                     return "Période orbitale : " + d.orbitalPeriod + " jours"
                 })
             texto.append("text")
-                .attr("dy", "16em")
+                .attr("dy", "14em")
                 .attr("x", "" + width / 2 + "")
+                .attr("font-size", "21px")
                 .text(function(d) {
                     return "Période de révolution : " + d.orbitalVelocity + " jours"
                 })
             texto.append("text")
+                .attr("dy", "15em")
+                .attr("x", "" + width / 2 + "")
+                .attr("font-size", "21px")
+                .text(function(d) {
+                    return "Inclinaison sur l'écliptique : " + d.orbitalInclination + " jours"
+                })
+            texto.append("text")
+                .attr("dy", "16em")
+                .attr("x", "" + width / 2 + "")
+                .attr("font-size", "21px")
+                .text(function(d) {
+                    return "Température moyenne : " + d.meanTemperature + " °C"
+                })
+            texto.append("text")
                 .attr("dy", "17em")
                 .attr("x", "" + width / 2 + "")
+                .attr("font-size", "21px")
                 .text(function(d) {
-                    return "Période de révolution : " + d.orbitalVelocity + " jours"
+                    return "Pression de surface : " + d.surfacePressure + " Pa"
                 })
+            texto.append("text")
+                .attr("dy", "18em")
+                .attr("x", "" + width / 2 + "")
+                .attr("font-size", "21px")
+                .text(function(d) {
+                    return "Nombre de lunes : " + d.numberOfMoons
+                })
+
             d3.select(this)
                 .style("cursor", "pointer")
                 .transition()
