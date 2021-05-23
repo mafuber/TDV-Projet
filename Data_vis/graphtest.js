15741fb6b876a83dfca4461a314747650c923cf0
mafuber <mafuber@gmail.com>
2021-05-23 20:26:56 +0200

    Sources and comments

--- Data_vis/graphtest.js
+++ Data_vis/graphtest.js
@@ -1,9 +1,28 @@
+/*sources for elements-ground.json :
+Mercury:    https://www.universetoday.com/13992/composition-of-mercury/ 
+Venus :     http://mentallandscape.com/V_Venera11.htm (end of the document)
+Earth:      https://en.wikipedia.org/wiki/Earth
+Mars:       https://www.lpi.usra.edu/education/IYPT/Mars.pdf
+Jupiter:    composition is the same as atmospheric (gaz giant)
+Saturn:     composition is the same as atmospheric (gaz giant)
+Uranus:     composition is the same as atmospheric (gaz giant)
+Neptune:    composition is the same as atmospheric (gaz giant)
+
+
+Sources for the atmosphere:
+https://nssdc.gsfc.nasa.gov/planetary/factsheet/
+*/
+
+//Definition of a few global variables and attribute a class
+
 var graphType = 0;
 var scatterxOption = "numberOfMoons";
 var scatteryOption = "diameter";
+var OldSelection = 1;
 
 d3.selectAll("#scatterplotoptions").attr("class", "hidden");
 
+//Main Function to set the Graph type (atmo-ground OR scatterplot)
 function GraphTypeChange(Type, Option, Axis) {
     graphType = Type;
     if (Axis == 1) {
@@ -11,16 +30,18 @@ function GraphTypeChange(Type, Option, Axis) {
     } else if (Axis == 2) {
         scatteryOption = Option;
     }
-    GraphUpdate(1);
+    GraphUpdate(OldSelection);
 }
-
+//All the planets-size picture, names of them and selection to switch between the planet data 
 d3.json("planets.json").then(function(data) {
-    planets = data
 
+    //create the X axis
+    planets = data
     var planetScale = d3.scaleLinear()
         .domain([0, d3.max(planets, function(d) { return d.diameter; })])
         .range([0, 150]);
 
+    //and add a correct padding between the planets
     var padding = 30.2;
     planets.forEach(function(d, i) {
         if (i === 0) {
@@ -30,11 +51,13 @@ d3.json("planets.json").then(function(data) {
         }
     });
 
-    var OldSelection = 1;
+    //create the svg viewBox calling with the my_planets id
     var svg = d3.select("#my_planets")
         .append("svg")
         .attr("x", 0)
         .attr("viewBox", "0 -100 900 220");
+
+    // append the selector rectangles (grey) with a onclick function to switch the data in the further created function GraphUpdate
     svg.selectAll("rect")
         .data(planets)
         .enter()
@@ -57,9 +80,11 @@ d3.json("planets.json").then(function(data) {
             OldSelection = selection;
             GraphUpdate(selection);
         });
-
+    
+    //fill the first rectangle so the user undestands the interaction with the planets
     d3.select("rect[id='0']").attr("fill", "grey");
 
+    // append the name of the planets and enable the interaction also for switching planets
     svg.selectAll("text")
         .data(planets)
         .enter()
@@ -80,6 +105,8 @@ d3.json("planets.json").then(function(data) {
             OldSelection = selection;
             GraphUpdate(selection);
         });
+    
+    //append the circle for the planets with correct proportions (diameter) and add a gradient 
     svg.selectAll(".planetsGradient")
         .data(planets)
         .enter()
@@ -102,6 +129,9 @@ d3.json("planets.json").then(function(data) {
 
 });
 
+
+//create other svg (svg1) for the Graphs. Setting global variables for the sizes and make a viewBox
+
 var margin = { top: 20, right: 5, bottom: 20, left: 100 };
 var width = 1500 - margin.left - margin.right;
 var height = 200 - margin.top - margin.bottom;
@@ -111,8 +141,11 @@ var svg1 = d3.select("#my_dataviz1")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+// Function to create and update the graphs
 function GraphUpdate(choice) {
     d3.selectAll("#scatterplotoptions").attr("class", "hidden");
+
+    //first option: the ground data:
     if (graphType == 0) {
         d3.json("elements-ground.json").then(function(d) {
             if (choice == 1) {
@@ -132,8 +165,10 @@ function GraphUpdate(choice) {
             } else if (choice == 8) {
                 data = d.data8;
             }
+            // clearing text from last time function called
             svg1.selectAll("text").remove();
             width = 630;
+            //creating the x scale and y scale:
             var xScale = d3.scaleLinear()
                 .domain([0, 1])
                 .range([0, width]);
@@ -143,8 +178,8 @@ function GraphUpdate(choice) {
                 .range([0, height])
                 .padding(0.1);
 
+            //append the scales x & y
             svg1.selectAll("g").remove();
-
             var g2 = svg1.append("g");
 
             g2.append("g")
@@ -156,10 +191,12 @@ function GraphUpdate(choice) {
                 .style("color", "white")
                 .attr("transform", "translate(" + 0 + "," + height + ")");
 
+            //remove the circle if the scatter plot was called before
             svg1.selectAll("circle").remove();
-            var v = svg1.selectAll("rect")
-                .data(data)
 
+            // creating the rectangles with transition
+            var v = svg1.selectAll("rect")
+                .data(data);
             v.enter()
                 .append("rect")
                 .merge(v)
@@ -172,6 +209,7 @@ function GraphUpdate(choice) {
             v.exit()
                 .remove();
         })
+    // now we have the option with the atmospheric data and the idea is the same as before
     } else if (graphType == 1) {
         d3.json("elements-atmo.json").then(function(d) {
             if (choice == 1) {
@@ -232,11 +270,13 @@ function GraphUpdate(choice) {
             u.exit()
                 .remove();
         })
+        // here we have the scatter plot option but the user is not choosing the x axis
     } else if (graphType == 2) {
         console.log(scatterxOption + "  " + scatteryOption)
         test = "mass";
         d3.json("planets.json").then(function(d) {
             data = d;
+            //Find the range of values for all the axis
             yMax = d3.max(d, function(d) { return eval("d." + scatteryOption + "") });
             yMin = d3.min(d, function(d) { return eval("d." + scatteryOption + "") });
             var yScale = d3.scaleLinear()
@@ -247,11 +287,13 @@ function GraphUpdate(choice) {
             var xScale = d3.scaleBand()
                 .domain(d3.map(data, function(d) { return d.name }))
                 .range([0, width])
-
+            //clear the svg1 of any elements form before
             svg1.selectAll("g").remove();
             svg1.selectAll("rect").remove();
             svg1.selectAll("circle").remove();
             svg1.selectAll("text").remove();
+
+            //creating the axis
             var g1 = svg1.append("g");
 
             g1.append("g")
@@ -264,6 +306,7 @@ function GraphUpdate(choice) {
                 .style("font-family", "Dosis")
                 .attr("transform", "translate(" + 0 + "," + height + ")");
 
+            // making the x & y labels
             svg1.append("text")
                 .attr("x", width / 2.2)
                 .attr("y", height + 35)
@@ -278,6 +321,7 @@ function GraphUpdate(choice) {
                 .style("fill", "white")
                 .text("" + scatteryOption);
 
+            // placing the planets with a gradient
             var u = svg1.selectAll("rect")
                 .data(data)
             u.enter()
@@ -289,6 +333,9 @@ function GraphUpdate(choice) {
             u.exit()
             u.exit().remove();
         })
+        // Last option, the user can choose the axis and for that we change
+        //the class of #scatterplotoptions to visible and we repeat almost 
+        //the same process as for graphType==2
     } else if (graphType == 3) {
 
         d3.selectAll("#scatterplotoptions").attr("class", "visible");
@@ -351,4 +398,5 @@ function GraphUpdate(choice) {
         })
     }
 }
+//Calling the function when the page launches
 GraphUpdate(3);
